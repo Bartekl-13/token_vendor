f5bdfbc1eb8437446fe9f27477825c198089a0b9
root <root@BARTEK-PC>
2023-09-02 10:21:52 +0200

    changed readme

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,23 +1,78 @@
-pragma solidity 0.8.4;  //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
+pragma solidity 0.8.4; // Do not change the Solidity version as it negatively impacts submission grading
+
+/// @title Vendor Contract for Buying and Selling Tokens
+/// @author [Your Name]
+/// @notice This contract allows users to buy tokens with Ether and sell tokens for Ether.
+/// @dev All implemented functions are working correctly.
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
 contract Vendor is Ownable {
+    YourToken public yourToken;
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    event BuyTokens(
+        address indexed buyer,
+        uint256 amountOfEth,
+        uint256 amountOfTokens
+    );
+    event SellTokens(
+        address indexed sender,
+        uint256 amountOfEth,
+        uint256 amountOfTokens
+    );
 
-  YourToken public yourToken;
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress);
+    }
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+    uint256 public constant tokensPerEth = 100;
 
-  // ToDo: create a payable buyTokens() function:
+    /// @notice Buy tokens with ETH.
+    /// @dev The number of tokens purchased is calculated based on the provided ETH and the `tokensPerEth` rate.
+    function buyTokens() public payable {
+        uint256 numberOfTokens = msg.value * tokensPerEth;
+        bool sent = yourToken.transfer(msg.sender, numberOfTokens);
+        require(sent, "Transaction failed");
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+        emit BuyTokens(msg.sender, msg.value, numberOfTokens);
+    }
 
-  // ToDo: create a sellTokens(uint256 _amount) function:
+    /// @notice Withdraw ETH from the contract (onlyOwner).
+    function withdraw() public onlyOwner {
+        uint256 vendorBalance = address(this).balance;
+        address owner = msg.sender;
+        (bool sent, ) = owner.call{value: vendorBalance}("");
+        require(sent, "Failed to withdraw");
+    }
 
+    /// @notice Sell tokens for ETH.
+    /// @param amount The amount of tokens to sell.
+    function sellTokens(uint256 amount) public {
+        require(amount > 0, "Must sell a token amount greater than 0");
+
+        address sender = msg.sender;
+        uint256 senderBalance = yourToken.balanceOf(sender);
+        require(senderBalance >= amount, "Not enough balance");
+        require(
+            yourToken.allowance(sender, address(this)) >= amount,
+            "Allowance is insufficient"
+        );
+
+        uint256 amountOfEth = amount / tokensPerEth;
+        uint256 vendorEthBalance = address(this).balance;
+        require(
+            vendorEthBalance >= amountOfEth,
+            "Vendor does not have enough ETH"
+        );
+
+        bool sent = yourToken.transferFrom(sender, address(this), amount);
+        require(sent, "Failed to transfer tokens");
+
+        (bool ethSent, ) = sender.call{value: amountOfEth}("");
+        require(ethSent, "Failed to send back ETH");
+
+        emit SellTokens(sender, amountOfEth, amount);
+    }
 }
--- packages/hardhat/deploy/00_deploy_your_token.js
+++ packages/hardhat/deploy/00_deploy_your_token.js
@@ -16,29 +16,32 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
 
   const yourToken = await ethers.getContract("YourToken", deployer);
 
-  // Todo: transfer tokens to frontend address
-  // const result = await yourToken.transfer("0x18fFE4dADcCe63A074Ef9cfe327cAb9AD4Ad9f76", ethers.utils.parseEther("1000") );
+  // // Todo: transfer tokens to frontend address
+  // const result = await yourToken.transfer(
+  //   "0xBA64e078F47DA3D2e059c9c1C61C8e6fB7f54023",
+  //   ethers.utils.parseEther("1000")
+  // );
 
-  // ToDo: To take ownership of yourContract using the ownable library uncomment next line and add the
-  // address you want to be the owner.
-  // yourContract.transferOwnership(YOUR_ADDRESS_HERE);
+  // // ToDo: To take ownership of yourContract using the ownable library uncomment next line and add the
+  // // address you want to be the owner.
+  // yourContract.transferOwnership(0xba64e078f47da3d2e059c9c1c61c8e6fb7f54023);
 
-  // if you want to instantiate a version of a contract at a specific address!
+  // // if you want to instantiate a version of a contract at a specific address!
   // const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A");
 
-  // If you want to send value to an address from the deployer
+  // // If you want to send value to an address from the deployer
   // const deployerWallet = ethers.provider.getSigner()
   // await deployerWallet.sendTransaction({
   //   to: "0x34aA3F359A9D614239015126635CE7732c18fDF3",
   //   value: ethers.utils.parseEther("0.001")
   // })
 
-  // If you want to send some ETH to a contract on deploy (make your constructor payable!);
+  // // If you want to send some ETH to a contract on deploy (make your constructor payable!);
   // const yourContract = await deploy("YourContract", [], {
   // value: ethers.utils.parseEther("0.05")
   // });
 
-  // If you want to link a library into your contract:
+  // // If you want to link a library into your contract:
   // const yourContract = await deploy("YourContract", [], {}, {
   //  LibraryName: **LibraryAddress**
   // });
